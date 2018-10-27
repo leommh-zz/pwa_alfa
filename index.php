afb69550a4a7d141a8447f06760db625f2245b53
Leonardo Morini <leo.morini.nn@gmail.com>
2018-10-27 09:14:49 -0300

    add -> scripts

--- index.php
+++ index.php
@@ -5,9 +5,16 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Sub Sub Marino.com</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+    <!-- CSS -->
     <link rel="stylesheet" type="text/css" href="css/materialize.min.css" />
     <link rel="stylesheet" type="text/css" href="css/style.css" />
-    <script src="main.js"></script>
+    
+    <!-- JS/SCRIPT -->
+    <script src="js/jquery-3.3.1.min.js"></script>
+    <script src="js/jquery.cycle2.min.js"></script>
+    <script src="js/materialize.min.js"></script>
+
 </head>
 <body>
     
